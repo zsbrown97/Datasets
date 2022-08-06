6360bb8773983f32f31a00ff33973f8f1c33e965
zsbrown97 <zack@jeffandbetsy.net>
2022-08-06 00:11:09 -0500

    fixed most of the deckbox_formatter script, added Blockbuster data

--- MTG/Deck Analyses/deckbox_formatter.py
+++ MTG/Deck Analyses/deckbox_formatter.py
@@ -1,35 +1,23 @@
 import pandas as pd
 
 
-def bring(nburl):
-    df = pd.DataFrame(pd.read_csv(nburl))
-    df = df[df['Section'].str.contains('scratchpad|sideboard') == False]
-    return df
+def read(x):
+    df = pd.DataFrame(pd.read_csv(x))
+    return df[df['Section'].str.contains('scratchpad|sideboard') == False]
 
 
-def dropCols(df):
-    x = df[df['Section'].str.contains('scratchpad|sideboard') == False]
-    x.reset_index(drop=True) \
-        .set_index(x['Name']) \
-        .drop(['Name', 'Section', 'Image URL', 'Card Number', 'Condition', 'Language', 'Foil', 'Signed', 'Artist Proof',
-               'Altered Art', 'Misprint'], axis=1)
+def dropColumns(x):
+    return x.reset_index(drop=True).set_index(x['Name']).drop(['Name', 'Section', 'Image URL', 'Card Number', 'Condition', 'Language', 'Foil', 'Signed', 'Artist Proof', 'Altered Art', 'Misprint'], axis=1)
+    # Dropped Image URL until I can find a way to display images
+
+def creatures(x):
+    x = x[x['Type'].str.contains('Creature')].drop(['Count'], axis=1)
+    x.replace('(?:Creature - )','', regex=True, inplace=True)
     return x
 
+def lands(x):
+    return x[x['Type'].str.contains('Land')].drop(['Cost'], axis=1)
 
-def formatCreatures(df):
-    creatures = df[df['Type'].str.contains('Creature')].drop(['Count'], axis=1)
-    creatures.replace('(?:Creature - )', '', regex=True, inplace=True)
-    return creatures
+def splitTables(x, y):
+    return x[x['Type'].str.contains(y)].drop(['Count', 'Type'], axis=1)
 
-
-def format(df, cardType):
-    formattedDf = df[df['Type'].str.contains(cardType)]
-
-    if (cardType == 'Creature'):
-        formattedDf.drop(['Count'], axis=1)
-    elif (cardType == 'Land'):
-        formattedDf.drop(['Cost'], axis=1)
-    else:
-        formattedDf.drop(['Count', 'Type'], axis=1)
-
-    return formattedDf
